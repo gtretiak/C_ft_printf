863f904983ef3c1d9adb9a08725fa6c8d51179f8
Georgii Tretiakov <george22021988@gmail.com>
2025-07-17 16:46:05 +0100

    Update ft_printf.c

--- Sources/ft_printf.c
+++ Sources/ft_printf.c
@@ -15,24 +15,24 @@
 
 int	ft_printf(const char *format, ...)
 {
-	va_list	args;
-	int		count;
+	va_list	args; // to handle variable arguments
+	int		count; // track th total number of characters printed
 
-	va_start(args, format);
+	va_start(args, format); // initializing va_list to begin retrieving the variable arguments from format
 	count = 0;
-	if (!format)
+	if (!format) // if nothing has passed to the function
 		return (write (1, "(null)", 6));
 	while (*format)
 	{
-		if (*format == '%')
-			count += ft_check_chars(*(++format), args);
-		else
+		if (*format == '%') // once we found % sign...
+			count += ft_check_chars(*(++format), args); // ... we check the following char
+		else // otherwise just printing as is
 		{
 			ft_putchar_fd(*(char *)format, 1);
 			count++;
 		}
 		format++;
 	}
-	va_end(args);
+	va_end(args); // cleaning up for va_list
 	return (count);
 }
