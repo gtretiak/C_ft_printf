d1f127576b2d89ba572c0aece7d86491e819ea91
Georgii Tretiakov <george22021988@gmail.com>
2025-07-17 16:59:41 +0100

    Update ft_take_address.c

--- Sources/ft_take_address.c
+++ Sources/ft_take_address.c
@@ -16,13 +16,13 @@
 int	ft_take_address(void *arg)
 {
 	int			count;
-	uintptr_t	address;
+	uintptr_t	address; // specific data type for storing a pointer safely
 
 	count = 0;
 	if (!arg)
-		return (write(1, "(nil)", 5));
-	address = (uintptr_t)arg;
-	count += write(1, "0x", 2);
-	count += ft_print_address(address);
+		return (write(1, "(nil)", 5)); // pointer is NULL
+	address = (uintptr_t)arg; // to process we need an int data type, so we cast generic pointer to uintptr_t
+	count += write(1, "0x", 2); // prefix for hexadecimal pointer representation
+	count += ft_print_address(address); // printing through recursion and digit-by-digit output in lowercase
 	return (count);
 }
